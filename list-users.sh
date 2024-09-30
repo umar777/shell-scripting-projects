db68aa2e5053c0a7987a19f0b046644e734c6b6c
omar farooqui <umarbinmuhammed@gmail.com>
2024-09-30 21:55:00 +0000

    script for listing users in github repo

--- list-users.sh
+++ list-users.sh
@@ -1,1 +1,36 @@
 #!/bin/bash
+
+
+API_URL="https://api.github.com"
+
+USERNAME=$username
+TOKEN=$token
+
+REPO_OWNER=$1
+REPO_NAME=$2
+
+function github_api_get {
+	local endpoint="$1"
+	local url="${API_URL}/${endpoint}"
+
+	curl -s -u "${USERNAME}:${TOKEN}" "$url"
+}
+
+function list_users {
+	local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
+	
+	
+	collabrators="$(github_api_get "$endpoint" | jq -r '.[] | select(.permissions.pull == true) | .login')"
+	
+	if [[ -z "$collabrators"  ]]; then
+		echo "No users with read access foung for ${REPO_OWNER}/${REPO_NAME}."
+	else
+		echo "Users with read access found for ${REPO_OWNER}/${REPO_NAME}:"
+		echo "$collabrators"
+	fi
+
+}
+
+echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
+list_users
+
